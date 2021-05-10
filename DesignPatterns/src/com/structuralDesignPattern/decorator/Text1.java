47d045dbedc835d7ba1e264ef5685c2c78c17f5d
Vermas-Dell <Vermas-Dell@Dell>
2021-05-11 02:07:54 +0530

    Test Class Commit

---
+++ DesignPatterns/src/com/structuralDesignPattern/decorator/Text1.java
@@ -0,0 +1,10 @@
+package com.structuralDesignPattern.decorator;
+
+public class Text1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test 1");
+	}
+
+}
